0686be2fc4134bbe4d82c67e7d0fc0aa9b8e82cc
ltrix07 <skotarenko138@gmail.com>
2024-05-27 16:58:06 +0300

    drop

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="google_sheets_api",
+    version="0.0.1",
+    description="A handy library for interacting with the Google Sheets API.",
+    author="ltrix07",
+    author_email="ltrix02@gmail.com",
+    url="https://github.com/ltrix07/google_sheet_api_utils",
+    packages=find_packages()
+)
