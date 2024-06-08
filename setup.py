fb571e2ea965478f59e4e059997f5f6470a1bfc8
ltrix07 <skotarenko138@gmail.com>
2024-06-08 12:32:10 +0300

    add methods get_columns_names, get_column_index_by_name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="google_sheets_api",
-    version="0.0.2",
+    version="0.0.3",
     description="A handy library for interacting with the Google Sheets API.",
     author="ltrix07",
     author_email="ltrix02@gmail.com",
