67304431353587f7f5ed1848ec0584f0132ea86b
ltrix07 <skotarenko138@gmail.com>
2024-06-19 12:25:02 +0300

    update_sheet_by_indices fix ssl.SSLEOFError

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="google_sheets_api",
-    version="0.1.4",
+    version="0.1.5",
     description="A handy library for interacting with the Google Sheets API.",
     author="ltrix07",
     author_email="ltrix02@gmail.com",
