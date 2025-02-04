2f93dc82c427a37cbf302f87ee77aa0abd231bab
ltrix07 <skotarenko138@gmail.com>
2025-02-04 14:41:23 +0200

    update for old versions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print(requirements)
 
 setup(
     name="google_sheets_api",
-    version="0.3.2",
+    version="0.3.3",
     description="A handy library for interacting with the Google Sheets API.",
     author="ltrix07",
     author_email="ltrix02@gmail.com",
